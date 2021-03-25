741c8187643916d34643597b2a4be2e0e7b8ae58
Chirinoxi <ignacio.chirino@alumnos.ucn.cl>
2021-03-25 08:51:21 -0300

    feat(image_processor.py): se añaden métodos y código para "eliminar" las imágenes.

--- image_processor.py
+++ image_processor.py
@@ -9,7 +9,6 @@ class ImageProcessor:
 	def __init__(self, images_dir):
 		self.current_dir = images_dir	
 
-
 	def manhattan_distance(self, x, y):
 		""" Calcula la 'distancia de manhattan' entre dos vectores
 
@@ -54,10 +53,6 @@ class ImageProcessor:
 				img = Image.open(directory+files[i]).convert('RGB')
 				img = img.resize((30, 30))
 				new_images.append(img)
-
-		#new_images = images.copy() #Creamos una copia de la variable images.
-		#new_images = list(map(lambda x: Image.open(directory+x).convert('RGB'), images)) # Convertimos todas las imágenes a formato PIL con 3 canales.
-		#new_images = list(map(lambda x: x.resize((30, 30)), new_images)) # Cambiamos el tamaño de todas las imágenes a 300x300.
 		return new_images
 
 	def min_max_norm(self, data):
@@ -81,21 +76,18 @@ class ImageProcessor:
 
 
 	def delete_images(self, directory):
-
-		self.delete_corrupted_images(directory)
 		files = sorted(os.listdir(directory)) # Leemos los archivos el directorio
-		files = self.delete_directories(files, directory)
+		files = self.delete_directories(files, directory) # Eliminamos los directorios del arreglo
 		splitted_dir = directory.split('/')
 		current_food = splitted_dir[-2]
 		pil_files = self.resize_images(files, directory) # Creamos un arreglo con los archivos en formato PIL y con dimensionsionalidad 300x300
-
 		for (idx, file) in enumerate(pil_files): # Podríamos recorrer pil_files
 			print("Imagen pivote: ", files[idx])
 			pivot_image = np.asarray(file)
 			pivot_image = self.min_max_norm(pivot_image)
 			rango = len(files)
 			i = idx
-			while i < rango:
+			while (i < rango):
 				if (not os.path.isdir(directory+files[i])):
 					img_name = files[i]
 					deleted_imgs_dir = directory + current_food + "_eliminados"
@@ -107,7 +99,6 @@ class ImageProcessor:
 						if(not os.path.exists(deleted_imgs_dir)):
 							os.mkdir(deleted_imgs_dir)
 							print("Directorio creado {} !".format(deleted_imgs_dir))
-							
 						print("Eliminando imagen: ", full_img_name)
 						shutil.move(directory+img_name, directory+current_food+"_eliminados/"+img_name)
 						#os.remove(directory+img_name) # Si queremos eliminar la imagen descomentar esta línea
@@ -117,19 +108,37 @@ class ImageProcessor:
 				i+=1
 
 	def delete_repeated_images(self):
+		self.delete_not_images(self.current_dir) # eliminamos los archivos que NO son imágenes
 		files = sorted(os.listdir(self.current_dir)) # Leemos los archivos el directorio
 		for file in files:
 			element = self.current_dir+file+'/'
 			if (os.path.isdir(element)):
-				self.rename_files(element)
+				#self.rename_files(element)
 				print("Eliminando imágenes del directorio: ", element)
+				self.delete_corrupted_images(element)
 				self.delete_images(element)
 			else:
 				continue
 
+	def delete_not_images(self, directory):
+		folders = sorted(os.listdir(directory))
+		for i in range(len(folders)):
+			curr_folder_name = folders[i]
+			current_folder_path = directory + curr_folder_name + "/"
+			folder_images = sorted(os.listdir(current_folder_path))
+			for (idx, file) in enumerate(folder_images):
+				full_file_name = current_folder_path + file
+				if(os.path.isdir(full_file_name)):
+					continue
+				else:
+					try:
+						im=Image.open(full_file_name)
+					except IOError:
+						print("Eliminando: ", full_file_name)
+						os.remove(full_file_name)
+
 	def delete_corrupted_images (self, directory):
 		files = os.listdir(directory);
-
 		for file in files:
 			if (os.path.isdir(directory + file)):
 				continue
@@ -138,7 +147,6 @@ class ImageProcessor:
 					img = Image.open(directory+file)
 					img.verify() #
 				except Exception:
-					print("File: ", file)
+					print("Eliminando imagen corrupta: ", file)
 					os.remove(directory+file) # Si queremos eliminar la imagen descomentar esta línea
-
-		print("Eliminamos imagenes corruptas !")
+		print("Se han eliminado todas las imágenes corruptas !")
